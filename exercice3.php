1e469aa2d8fe5919bc0755c54bc8a27d6f4af4e8
Anthony Laplane <anthony.laplane@gmail.com>
2025-01-06 18:09:15 +0100

    rename

--- exercice3.php
+++ exercice3.php
@@ -2,7 +2,7 @@
 
 /**
  * ÉNONCÉ :
- * Importer le fichier de l'exercice1 pour avoir accès à la classe Person
+ * Importer le fichier de l'exercice2 pour avoir accès à la classe Person
  * Écrivez une classe nommée Employee qui hérite de la classe Person
  * et ajoute les propriétés salary et position.
  * Surchager le constructeur et appeler le constructeur parent
