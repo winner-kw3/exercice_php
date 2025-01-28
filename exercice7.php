ac986d95444736b520585fb5567ff764496bdc88
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-01-28 16:57:19 +0100

    devoir

--- exercice7.php
+++ exercice7.php
@@ -10,4 +10,25 @@
 */
 class BankAccount {
     private $balance;
-?>
+    public function __construct($balance = 0) {
+        $this->balance = $balance;
+        }
+    public function deposit($amount) {
+            $this->balance += $amount;
+            echo "Vous avez déposé $amount. Votre nouveau solde est de $this->balance.";
+            }
+    public function withdraw($amount) {
+                if ($amount > $this->balance) {
+                    echo "Vous n'avez pas suffisamment d'argent dans votre compte.\n";
+                    }
+                    else {
+                        $this->balance -= $amount;
+                        echo "Vous avez retiré $amount. Votre nouveau solde est de $this->balance";
+                        }
+                        }
+    }
+    $account = new BankAccount(100);
+    $account->deposit(50);
+    $account->withdraw(75);
+    $account->withdraw(150);
+                        ?>
